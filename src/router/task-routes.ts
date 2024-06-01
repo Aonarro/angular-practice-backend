06436985a71e5e0f3823bb848d25533bfead6327
Nikolay <nikolay.tyshchenko04@gmail.com>
2024-06-02 00:30:08 +0300

    finish angular project

--- src/router/task-routes.ts
+++ src/router/task-routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express'
-import { createTask, getTasksByCustomerId } from '../services/task-service'
+import {
+	createTask,
+	deleteTask,
+	doneTask,
+	getTasksByCustomerId,
+} from '../services/task-service'
 
 const router = Router()
 
 router.get('/:customerId', getTasksByCustomerId)
 router.post('/new', createTask)
+router.delete('/:taskId', deleteTask)
+router.put('/:taskId', doneTask)
 
 export default router
--- src/services/task-service.ts
+++ src/services/task-service.ts
@@ -3,7 +3,7 @@ import Task from '../models/Task'
 
 export const getTasksByCustomerId = async (req: Request, res: Response) => {
 	try {
-		console.log(req.params.customerId)
+		// console.log(req.params.customerId)
 		const id = req.params.customerId
 		const tasks = await Task.find({
 			customerId: id,
@@ -20,6 +20,33 @@ export const createTask = async (req: Request, res: Response) => {
 		const newTask = new Task({ description, date, customerId, done })
 		await newTask.save()
 		res.status(201).json(newTask)
+	} catch (error) {
+		res.status(500).json({ error: 'Internal server error' })
+	}
+}
+
+export const deleteTask = async (req: Request, res: Response) => {
+	try {
+		const { taskId } = req.params
+		const response = await Task.deleteOne({
+			_id: taskId,
+		})
+		res.status(200).json('Task deleted')
+	} catch (error) {
+		res.status(500).json({ error: 'Internal server error' })
+	}
+}
+
+export const doneTask = async (req: Request, res: Response) => {
+	try {
+		const { taskId } = req.params
+
+		await Task.findByIdAndUpdate(
+			{ _id: taskId },
+			{ done: req.body.done },
+			{ new: true, runValidators: true }
+		)
+		res.status(200).json('Task is done')
 	} catch (error) {
 		res.status(500).json({ error: 'Internal server error' })
 	}
